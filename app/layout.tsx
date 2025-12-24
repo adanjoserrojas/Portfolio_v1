3fbc6fb999db7017a0ec7b6e824592b7a8a0b091
adanjoserrojas <adanrojas224@gmail.com>
2025-12-23 23:54:53 -0500

    Fixed Meta Data pt.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,10 +13,38 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'Portfolio | Adan Rojas ',
-  description: 'Minimalist portfolio showcasing creative Full-Stack Dev solutions :)',
-  keywords: ['portfolio', 'web developer', 'UI/UX', 'TypeScript', 'Next.js', 'creative development', 'Full-Stack Developer', 'SWE'],
-  authors: [{ name: 'Adan Rojas' }],
+    title: "Portfolio | Adan Rojas",
+    description:
+        "An aspiring software engineer passionate about building impactful and performant software solutions. Currently a student at the University of Central Florida, actively seeking opportunities to contribute to innovative projects and collaborate with like-minded professionals in the tech industry.",
+    keywords: [
+        "Adan Rojas",
+        "Software Engineer",
+        "UCF",
+        "Knight Hacks",
+        "Full-Stack Developer",
+        "Dahiana Rojas",
+        "Intern",
+        "UCF",
+        "University of Central Florida",
+        "Workshop Team Member",
+        "Python",
+        "React",
+        "Next.js",
+        "Computer Science",
+    ],
+    authors: [{ name: "Adan Rojas" }],
+    creator: "Adan Rojas",
+    publisher: "Adan Rojas",
+
+    openGraph: {
+        type: "website",
+        locale: "en_US",
+        url: "https://www.4dan.dev",
+        title: "Portfolio | Adan Rojas",
+        description:
+            "An aspiring software engineer passionate about building impactful and performant software solutions. Currently a student at the University of Central Florida, actively seeking opportunities to contribute to innovative projects and collaborate with like-minded professionals in the tech industry.",
+        siteName: "Adan Rojas",
+    },
 };
 
 export const viewport = 'width=device-width, initial-scale=1'
